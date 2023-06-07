d6e6ac2058c8e4a06268277391e9eaf3e7184cda
Katorligan <katorligan@gmail.com>
2023-06-07 19:20:06 +0200

    feat: style header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/LOGO.png';
+import './index.css';
 
 function Header() {
 	return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 import Home from './pages/Home';
 import House from './pages/House';
 import About from './pages/About';
