719535fcbd346a203addef99139512a384d0dc52
Katorligan <katorligan@gmail.com>
2023-06-08 12:05:18 +0200

    feat: add banner component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,12 @@
+import Banner from '../../components/Banner';
+import bannerPicture from '../../assets/images/banner_home.png';
+
 function Home() {
-	return <div>Accueil</div>;
+	return (
+		<div>
+			<Banner picture={bannerPicture} alt="Falaises de bord de mer" title="Chez vous, partout et ailleurs" />
+		</div>
+	);
 }
 
 export default Home;
