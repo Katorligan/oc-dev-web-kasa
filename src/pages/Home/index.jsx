a3d4b672a5313586ee4ae5bf9cb6b9119d26469d
Katorligan <katorligan@gmail.com>
2023-06-08 17:35:05 +0200

    feat: fetch home cards from data file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from 'react';
 import Banner from '../../components/Banner';
 import Card from '../../components/Card';
-import './index.css';
 import bannerPicture from '../../assets/images/banner_home.png';
+import './index.css';
 
 function Home() {
+	const [houses, setHouses] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const response = await fetch('./logements.json');
+				const data = await response.json();
+				setHouses(data);
+			} catch (e) {
+				console.log(e);
+			}
+		}
+		fetchData();
+	}, []);
+
 	return (
 		<main>
 			<Banner picture={bannerPicture} alt="Falaises de bord de mer" title="Chez vous, partout et ailleurs" />
 			<div className="card-wrapper">
-				<Card picture={bannerPicture} title="Ceci est le titre de la location" />
+				{houses.map((house) => (
+					<Card key={house.id} picture={house.cover} title={house.title} id={house.id} />
+				))}
 			</div>
 		</main>
 	);
