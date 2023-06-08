5794c4dd3be023439d3e6874086de74b0ce6248c
Katorligan <katorligan@gmail.com>
2023-06-08 10:44:31 +0200

    feat: add error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+import './index.css';
+
 function Error() {
-	return <div>Erreur</div>;
+	return (
+		<div className="error">
+			<h2>404</h2>
+			<h3>Oups! La page que vous demandez n'existe pas.</h3>
+			<Link to="/">Retourner sur la page d'accueil</Link>
+		</div>
+	);
 }
 
 export default Error;
