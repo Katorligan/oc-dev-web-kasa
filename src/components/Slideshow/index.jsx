5bcee5abb0538c3021b04154fb5113553544165c
Katorligan <katorligan@gmail.com>
2023-06-20 17:03:17 +0200

    feat: add slideshow component

---
+++ src/components/Slideshow/index.jsx
@@ -0,0 +1,20 @@
+import './index.css';
+import arrowLeft from '../../assets/images/arrow_left.png';
+import arrowRight from '../../assets/images/arrow_right.png';
+
+function Slideshow(props) {
+	const { pictures } = props;
+
+	return (
+		<div className="slideshow">
+			{pictures.map((picture, index) => (
+				<img className={index === 0 ? 'slide active' : 'slide'} src={picture} alt="Logement" />
+			))}
+			<img className="previous" src={arrowLeft} alt="Précédent" />
+			<img className="next" src={arrowRight} alt="Suivant" />
+			{pictures.length > 1 && <div className="slide-counter">1/{pictures.length}</div>}
+		</div>
+	);
+}
+
+export default Slideshow;
